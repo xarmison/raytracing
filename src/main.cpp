51980149e41fe07cabe4019801ef56cd316d3a00
vanluwin <richardsonsantiago@ufrn.edu.br>
2021-05-09 17:48:23 -0300

    New camera position

--- src/main.cpp
+++ src/main.cpp
@@ -44,7 +44,7 @@ int main(int argc, char* argv[]) {
 
     // World
     hittable_list world;
-    
+
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
     auto material_left   = make_shared<dielectric>(1.5);
@@ -52,11 +52,18 @@ int main(int argc, char* argv[]) {
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),  -0.4, material_left));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0), -0.45, material_left));
     world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
     // Camera
-    camera cam;
+    camera cam(
+        point3(-2, 2, 1), 
+        point3(0, 0, -1), 
+        vec3(0, 1, 0), 
+        60.0, 
+        aspect_ratio
+    );
 
     // Render
     std::cout << "P3\n" << im_width << " " << im_height << "\n255\n";
