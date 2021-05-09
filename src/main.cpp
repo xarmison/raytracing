df1709264a6654d6ea563ca5ff94bae877c6c320
vanluwin <richardsonsantiago@ufrn.edu.br>
2021-05-09 01:05:18 -0300

    Anti-Aliasing added

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,21 @@
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 
-#include "../include/utility.h"
+#include "../include/camera.h"
 #include "../include/color.h"
 #include "../include/hittable_list.h"
 #include "../include/sphere.h"
+#include "../include/utility.h"
 
-// Returns the color of the background (a simple gradient)
-double hit_sphere(const point3 &center, double radius, const ray &r) {
-    vec3 oc = r.origin() - center;
-
-    auto a = r.direction().length_squared();
-    auto half_b = dot(oc, r.direction());
-    auto c = oc.length_squared() - radius * radius;
-    
-    auto discriminant = half_b * half_b - a * c;
-
-    if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (-half_b - sqrt(discriminant)) / a;
-    }
-}
-
-color ray_color(const ray &r) {
-    double t = hit_sphere(point3(0, 0, -1), 0.5, r);
-    if (t > 0.0) {
-        vec3 normal = unit_vector(r.at(t) - vec3(0, 0, -1));
-
-        return 0.5 * color(
-            normal.x() + 1,
-            normal.y() + 1,
-            normal.z() + 1
-        );
+color ray_color(const ray &r, const hittable &world) {
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1.0, 1.0, 1.0));
     }
 
     vec3 unit_direction = unit_vector(r.direction());
     
-    t = 0.5 * (unit_direction.y() + 1.0);
+    auto t = 0.5 * (unit_direction.y() + 1.0);
 
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
@@ -48,17 +26,16 @@ int main(int argc, char* argv[]) {
     const auto aspect_ratio = 16.0 / 9.0;
     const int im_width  = 400;
     const int im_height = static_cast<int>(im_width / aspect_ratio);
+    const int samples_per_pixel = 100;
+
+    // World
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // Camera
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
+    camera cam;
 
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
-   
     // Render
     std::cout << "P3\n" << im_width << " " << im_height << "\n255\n";
 
@@ -68,18 +45,21 @@ int main(int argc, char* argv[]) {
             << " of " << im_height 
             << " - " << std::setw(5) <<  static_cast<double>(im_height - j) / im_height * 100 << "%" 
         << '\r';
-        
-        for (int i = 0; i < im_width; i++) {
-            auto u = double(i) / (im_width - 1);
-            auto v = double(j) / (im_height - 1);
-            
-            ray r(
-                origin,
-                lower_left_corner + u * horizontal + v * vertical - origin            
-            );
 
-            color pixel_color = ray_color(r);
-            write_color(std::cout, pixel_color);
+        for (int i = 0; i < im_width; i++) {
+            
+            color pixel_color(0, 0, 0);
+
+            for (int s = 0; s < samples_per_pixel; s++) {
+                auto u = double(i + random_double()) / (im_width - 1);
+                auto v = double(j + random_double()) / (im_height - 1);
+                
+                ray r = cam.get_ray(u, v);
+
+                pixel_color += ray_color(r, world);
+            }
+
+            write_color(std::cout, pixel_color, samples_per_pixel);
         }
     }
 
