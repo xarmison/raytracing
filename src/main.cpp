dad17a049abadd8f683c38271feb11bcd877147a
vanluwin <richardsonsantiago@ufrn.edu.br>
2021-05-12 18:14:47 -0300

    Added multiple scenes

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "../include/hittable_list.h"
 #include "../include/sphere.h"
 #include "../include/material.h"
+#include "../include/moving_sphere.h"
 
 color ray_color(const ray &r, const hittable &world, int depth) {
     hit_record rec;
@@ -36,11 +37,14 @@ color ray_color(const ray &r, const hittable &world, int depth) {
 hittable_list random_scene() {
     hittable_list world;
 
-    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+    // auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    // world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
 
-    for (int a = -11; a < 11; a++) {
-        for (int b = -11; b < 11; b++) {
+    auto checker = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, make_shared<lambertian>(checker)));
+
+    for (int a = -5; a < 5; a++) {
+        for (int b = -5; b < 5; b++) {
             auto choose_mat = random_double();
             point3 center(
                 a + 0.9 * random_double(),
@@ -56,7 +60,13 @@ hittable_list random_scene() {
                     auto albedo = color::random() * color::random();
                     
                     sphere_material = make_shared<lambertian>(albedo);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material)); 
+
+                    auto center_2 = center + vec3(0, random_double(0, 0.5), 0);
+                    world.add(make_shared<moving_sphere>(
+                        center, center_2, 
+                        0.0, 1.0,
+                        0.2, sphere_material
+                    )); 
                 
                 } else if (choose_mat < 0.95) {
                     // Metal
@@ -92,35 +102,142 @@ hittable_list random_scene() {
     world.add(make_shared<sphere>(
         point3(4, 1, 0),
         1.0,
-        make_shared<lambertian>(color(0.7, 0.6, 0.6))
+        make_shared<metal>(color(0.7, 0.6, 0.5), 0.0)
     ));
 
     return world;    
 }
 
+hittable_list two_spheres() {
+    hittable_list objects;
+
+    auto checker = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+
+    objects.add(make_shared<sphere>(
+        point3(0, -10, 0), 
+        10, 
+        make_shared<lambertian>(checker)
+    ));
+    
+    objects.add(make_shared<sphere>(
+        point3(0, 10, 0), 
+        10, 
+        make_shared<lambertian>(checker)
+    ));
+
+    return objects;
+}
+
+hittable_list two_perlin_spheres() {
+    hittable_list objects;
+
+    auto perlin_text = make_shared<noise_texture>(4);
+
+    objects.add(make_shared<sphere>(
+        point3(0, -1000, 0), 
+        1000, 
+        make_shared<lambertian>(perlin_text)
+    ));
+    
+    objects.add(make_shared<sphere>(
+        point3(0, 2, 0), 
+        2, 
+        make_shared<lambertian>(perlin_text)
+    ));
+
+    return objects;
+}
+
+hittable_list earth() {
+    auto globe = make_shared<sphere>(
+        point3(0.0, 0.0, 0.0),
+        2,
+        make_shared<lambertian>(make_shared<image_texture>("../textures/earthmap.jpg"))
+    );
+
+    return hittable_list(globe);
+}
+
 int main(int argc, char* argv[]) {
 
+    if (argc < 2) {
+        std::cerr << "Missing Arguments!\nUsage: " << argv[0] << " scene_id\n";
+        exit(-1);
+    }
+
     // Image
-    const auto aspect_ratio = 3.0 / 2.0;
-    const int im_width  = 1200;
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int im_width  = 1080;
     const int im_height = static_cast<int>(im_width / aspect_ratio);
-    const int samples_per_pixel = 500;
+    const int samples_per_pixel = 100;
     const int max_depth = 50;
 
     // World
-    auto world = random_scene();
+    hittable_list world;
+
+    point3 lookfrom;
+    point3 lookat;
+    auto vfov = 40.0;
+    auto aperture = 0.0;
+
+    int scene_id = atoi(argv[1]);
+    switch (scene_id) {
+        case 1:
+            std::cerr << "Rendering scene 1\n";
+            
+            world = random_scene();
+            lookfrom = point3(13,2,3);
+            lookat = point3(0,0,0);
+            vfov = 20.0;
+            aperture = 0.1;
+
+            break;
+        
+        case 2:
+            std::cerr << "Rendering scene 2\n";
+
+            world = two_spheres();
+            lookfrom = point3(13,2,3);
+            lookat = point3(0,0,0);
+            vfov = 20.0;
+
+            break;
+
+        case 3:
+            std::cerr << "Rendering scene 3\n";
+
+            world = two_perlin_spheres();
+            lookfrom = point3(13, 2, 3);
+            lookat = point3(0, 0, 0);
+            vfov = 20.0;
+
+            break;
+
+        case 4:
+            std::cerr << "Rendering scene 4\n";
+            
+            world = earth();
+            lookfrom = point3(13, 2, 3);
+            lookat = point3(0, 0, 0);
+            vfov = 20.0;
+
+            break;
+
+        default:
+            std::cerr << "Scene id not found!\n";
+            exit(-1);
+            break;
+    }
 
     // Camera
-    point3 lookfrom(13, 2, 3);
-    point3 lookat(0, 0, 0);
     vec3 vup(0, 1, 0);
     auto dist_to_focus = 10.0;
-    auto aperture = 0.1;
 
     camera cam(
         lookfrom, lookat, vup, 
-        20, aspect_ratio, 
-        aperture, dist_to_focus
+        vfov, aspect_ratio, 
+        aperture, dist_to_focus,
+        0.0, 1.0
     );
 
     // Render
