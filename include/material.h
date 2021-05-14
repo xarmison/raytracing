4ffc5ac363fef51ddb98b75196238a8935852948
vanluwin <richardsonsantiago@ufrn.edu.br>
2021-05-13 21:43:05 -0300

    Material for fog/smoke

--- include/material.h
+++ include/material.h
@@ -130,4 +130,26 @@ class diffuse_light : public material {
         }
 };
 
+class isotropic : public material {
+    public:
+        shared_ptr<texture> albedo;
+
+    public:
+        isotropic(color c) : albedo(make_shared<solid_color>(c)) {}
+        isotropic(shared_ptr<texture> a) : albedo(a) {}
+
+        virtual bool scatter(
+            const ray &r_in, 
+            const hit_record &rec, 
+            color &attenuation, ray &scattered
+        ) const override {
+            scattered = ray(rec.p, random_in_unit_sphere(), r_in.time());
+            attenuation = albedo->value(rec.u, rec.v, rec.p);
+
+            return true;
+        }
+
+
+};
+
 #endif // MATERIAL_H
