c707a4024e73d5880919b781c48f4a0654d46a46
vanluwin <richardsonsantiago@ufrn.edu.br>
2021-05-14 09:22:36 -0300

    Added new complex scene

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
 #include "../include/aarect.h"
 #include "../include/box.h"
 #include "../include/constant_medium.h"
+#include "../include/bvh.h"
 
 color ray_color(const ray &r, const color &background, const hittable &world, int depth) {
     hit_record rec;
@@ -254,6 +255,128 @@ hittable_list cornell_box_smoke() {
     return objects;
 }
 
+hittable_list final_scene() {
+    hittable_list boxes1;
+    auto ground = make_shared<lambertian>(color(0.48, 0.83, 0.53));
+
+    const int boxes_per_side = 20;
+    for (int i = 0; i < boxes_per_side; i++) {
+        for (int j = 0; j < boxes_per_side; j++) {
+            auto w  = 100.0;
+            
+            auto x0 = -1000.0 + i*w;
+            auto z0 = -1000.0 + j*w;
+            auto y0 = 0.0;
+
+            auto x1 = x0 + w;
+            auto y1 = random_double(1,101);
+            auto z1 = z0 + w;
+
+            boxes1.add(make_shared<box>(
+                point3(x0, y0, z0),
+                point3(x1, y1, z1),
+                ground
+            ));
+        }
+    }
+
+
+    hittable_list objects;
+
+    objects.add(make_shared<bvh_node>(boxes1, 0, 1));
+
+    // Light
+    objects.add(make_shared<xz_rect>(
+        123, 423, 147, 412, 554, 
+        make_shared<diffuse_light>(color(7, 7, 7))
+    ));
+
+    // Moving sphere
+    auto center1 = point3(400, 400, 200);
+    auto center2 = center1 + vec3(30, 0, 0);
+    objects.add(make_shared<moving_sphere>(
+        center1, center2, 
+        0, 1, 50, 
+        make_shared<lambertian>(color(0.7, 0.3, 0.1))
+    ));
+
+    // Glass sphere
+    objects.add(make_shared<sphere>(
+        point3(260, 150, 45), 
+        50, 
+        make_shared<dielectric>(1.5)
+    ));
+
+    // Metal sphere
+    objects.add(make_shared<sphere>(
+        point3(0, 150, 145), 
+        50, 
+        make_shared<metal>(color(0.8, 0.8, 0.9), 1.0)
+    ));
+
+    //Fog
+    auto boundary = make_shared<sphere>(
+        point3(360,150,145), 
+        70, 
+        make_shared<dielectric>(1.5)
+    );
+    objects.add(boundary);
+    objects.add(make_shared<constant_medium>(
+        boundary, 
+        0.2, 
+        color(0.2, 0.4, 0.9)
+    ));
+    boundary = make_shared<sphere>(
+        point3(0, 0, 0), 
+        5000, 
+        make_shared<dielectric>(1.5)
+    );
+    objects.add(make_shared<constant_medium>(
+        boundary, 
+        .0001, 
+        color(1,1,1)
+    ));
+
+    // Earth
+    objects.add(make_shared<sphere>(
+        point3(400, 200, 400),
+        100,
+        make_shared<lambertian>(make_shared<image_texture>("../textures/earthmap.jpg"))
+    ));
+
+    // Noise texture
+    objects.add(make_shared<sphere>(
+        point3(220, 280, 300),
+        80,
+        make_shared<lambertian>(
+            make_shared<noise_texture>(0.1)
+        )
+    ));
+
+    // Cube with random spheres
+    hittable_list boxes2;
+    auto white = make_shared<lambertian>(color(.73, .73, .73));
+    int ns = 1000;
+
+    for (int j = 0; j < ns; j++) {
+        boxes2.add(make_shared<sphere>(
+            point3::random(0, 165), 
+            10,
+            white
+        ));
+    }
+
+    objects.add(make_shared<translate>(
+        make_shared<rotate_y>(
+            make_shared<bvh_node>(boxes2, 0.0, 1.0), 
+            15
+        ),
+        vec3(-100, 270, 395)
+    ));
+
+    return objects;
+}
+
 int main(int argc, char* argv[]) {
 
     if (argc < 2) {
@@ -364,6 +487,22 @@ int main(int argc, char* argv[]) {
             lookat = point3(278, 278, 0);
             vfov = 40.0;
             
+            break;
+
+        case 8:
+            std::cerr << "Rendering scene 8\n";
+
+            world = final_scene();
+
+            aspect_ratio = 1.0;
+            im_width = 800;
+            samples_per_pixel = 10000;
+
+            background = color(0.0, 0.0, 0.0);
+            lookfrom = point3(478, 278, -600);
+            lookat = point3(278, 278, 0);
+            vfov = 40.0;
+
             break;
 
         default:
