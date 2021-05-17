ea6d29d772d6b8d7747d5b1b3f38f0438801be4d
Richardson Santiago <richardsonsantiago@ufrn.edu.br>
2021-05-17 11:26:29 -0300

    Func to return the color value as a string

--- include/color.h
+++ include/color.h
@@ -2,6 +2,7 @@
 #define COLOR_H
 
 #include <iostream>
+#include <sstream>
 
 #include "utility.h"
 #include "vec3.h"
@@ -27,6 +28,30 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
     out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
         << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
         << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
+}
+
+std::string get_color(color pixel_color, int samples_per_pixel) {
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    // Replace NaN components with zero
+    if (r != r) r = 0.0;
+    if (g != g) g = 0.0;
+    if (b != b) b = 0.0;
+
+    // Divide the color by the number of samples and gamma-correct for gamma = 2
+    auto scale = 1.0 / samples_per_pixel;
+    r = sqrt(r * scale);
+    g = sqrt(g * scale);
+    b = sqrt(b * scale);
+
+    std::stringstream ss;
+    ss << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
+        << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
+        << static_cast<int>(256 * clamp(b, 0.0, 0.999));
+
+    return ss.str();
 }
 
 #endif  // COLOR_H
